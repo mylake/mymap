68d385a8226bcd7beed0d3a5a605548f8b6c7a16
mylake <ilake.chang@gmail.com>
2017-07-08 11:22:27 +0800

    remove jwt_authenticaiton model

--- app/models/jwt_authentication.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class JwtAuthentication < ApplicationRecord
-  def self.registered?(key, secret)
-    where(key: key, secret: secret).count > 0
-  end
-end
--- spec/factories/jwt_authentications.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryGirl.define do
-  factory :jwt_authentication do
-    name    'esther'
-    key     'keylalala'
-    secret  'secretlalala'
-  end
-end
